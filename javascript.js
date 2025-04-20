160b649226f23ea55496f47f005b0ec48b74382c
bleepblue <lawrence.charlesworth0@gmail.com>
2025-04-20 21:23:24 +0100

    website modified for summer show

--- javascript.js
+++ javascript.js
@@ -1,46 +1,35 @@
         let dropdownMenu = false;
-        let scheduleMenu = false;
         let entryMenu = false;
         let phoneMenu = false;
-
-
-        
-        
 
         function closeDropdown()
         {
             document.querySelector(".dropdown-content").style.height="0";
             dropdownMenu = false;
-            scheduleMenu = false;
             entryMenu = false;
-            document.querySelector(".schedule-dropdown").style.height="0";
             document.querySelector(".entry-dropdown").style.height="0";
-            document.querySelector(".show").classList.remove("link-selected");
-            document.querySelector(".show").classList.add("link");
-            document.querySelector(".schedule").classList.remove("sub-link-selected");
-            document.querySelector(".schedule").classList.add("sub-link");
+            document.querySelector("#show").classList.remove("link-selected");
+            document.querySelector("#show").classList.add("link");
             document.querySelector(".entry").classList.remove("sub-link-selected");
             document.querySelector(".entry").classList.add("sub-link");
-            if(window.innerWidth <= 900)
+            if(window.innerWidth <= 1100)
                 {
                     navHeight();
                     
                 }
-            document.cookie = "entry=false; path=/;";
-            document.cookie = "schedule=false; path=/;";
         }
 
         function openDropdown()
         {
-            if(window.innerWidth <= 900)
+            if(window.innerWidth <= 1100)
                 {
                     document.querySelector(".dropdown-content").style.transition="none";
                 }
             document.querySelector(".dropdown-content").style.height="138px";
             dropdownMenu = true;
-            document.querySelector(".show").classList.add("link-selected");
-            document.querySelector(".show").classList.remove("link");
-            if(window.innerWidth <= 900)
+            document.querySelector("#show").classList.add("link-selected");
+            document.querySelector("#show").classList.remove("link");
+            if(window.innerWidth <= 1100)
                 {
                     
                     
@@ -50,148 +39,45 @@
             
         }
 
-        function closeSchedule()
-        {
-            scheduleMenu = false;
-            document.querySelector(".schedule-dropdown").style.height="0";
-            if (entryMenu)
-                {
-                    document.querySelector(".dropdown-content").style.height="230px";
-                }
-            else
-                {
-                    document.querySelector(".dropdown-content").style.height="138px";
-                }
-            document.querySelector(".schedule").classList.remove("sub-link-selected");
-            document.querySelector(".schedule").classList.add("sub-link");
-            if(window.innerWidth <= 900)
-                {
-                    navHeight();
-                }
-            document.cookie = "schedule=false; path=/;";
-        }
+      
 
-        function openSchedule()
-        {
-            if(window.innerWidth <= 900)
-                {
-                    document.querySelector(".dropdown-content").style.transition="none";
-                }
-            scheduleMenu = true;
-            document.querySelector(".schedule-dropdown").style.height="92px";
-            if (entryMenu)
-                {
-                    document.querySelector(".dropdown-content").style.height="322px";
-                }
-            else
-                {
-                    document.querySelector(".dropdown-content").style.height="230px";
-                }
-            document.querySelector(".schedule").classList.add("sub-link-selected");
-            document.querySelector(".schedule").classList.remove("sub-link");
-            if(window.innerWidth <= 900)
-                {
-                    
-                    openNav();
-                        
-                }
-            document.cookie = "schedule=true; path=/;";
-        }
+      
 
         function closeEntry()
         {
             entryMenu = false;
             document.querySelector(".entry-dropdown").style.height="0";
-            if (scheduleMenu)
-                {
-                    document.querySelector(".dropdown-content").style.height="230px";
-                }
-            else
-                {
-                    document.querySelector(".dropdown-content").style.height="138px";
-                }
+            document.querySelector(".dropdown-content").style.height="138px";  
             document.querySelector(".entry").classList.remove("sub-link-selected");
             document.querySelector(".entry").classList.add("sub-link");
-            if(window.innerWidth <= 900)
+            if(window.innerWidth <= 1100)
                 {
                     
                     navHeight();
                         
                 }
-            document.cookie = "entry=false; path=/;";
         }
 
         function openEntry()
         {
-            if(window.innerWidth <= 900)
+            if(window.innerWidth <= 1100)
                 {
                     document.querySelector(".dropdown-content").style.transition="none";
                 }
             entryMenu = true;
             document.querySelector(".entry-dropdown").style.height="92px";
-            if (scheduleMenu)
-                {
-                    document.querySelector(".dropdown-content").style.height="322px";
-                }
-            else
-                {
-                    document.querySelector(".dropdown-content").style.height="230px";
-                }
+            document.querySelector(".dropdown-content").style.height="230px";               
             document.querySelector(".entry").classList.add("sub-link-selected");
             document.querySelector(".entry").classList.remove("sub-link");
-            if(window.innerWidth <= 900)
+            if(window.innerWidth <= 1100)
                 {
                     
                     openNav();
                         
                 }
-            document.cookie = "entry=true; path=/;";   
         }
 
-        function checkMenu ()
-        {
-            let cookies = document.cookie;
-            let cookieList = cookies.split(";");
-            let i = 0;
-            let menuList =[];
-            cookieList.forEach(cookie=>{
-                menuList[i] = cookie.split("=");
-                i++;
-            });
-            menuList.forEach(menu=>{
-                if(menu[0].match("entry"))
-                {
-                    if(menu[1] == "true")
-                    {
-                        if(!entryMenu)
-                        {
-                          
-                            document.querySelector(".entry-dropdown").style.transition="none";
-                            openEntry();
-                            document.querySelector(".entry-dropdown").style.transition="height 0.5s";
-                           
-
-                        }
-                    }
-                }
-                else if(menu[0].match("schedule"))
-                {
-                    if(menu[1] == "true")
-                    {
-                        if(!scheduleMenu)
-                        {
-                            
-                            document.querySelector(".schedule-dropdown").style.transition="none";
-                            openSchedule();
-                            document.querySelector(".schedule-dropdown").style.transition="height 0.5s";
-                            
-
-                        }
-                    }
-                }
-            })
-        }
-
+       
 
 
 
@@ -208,19 +94,7 @@
 
         }
 
-        function scheduleDropdown()
-        {
-    
-            if (scheduleMenu)
-                {
-                   closeSchedule()
-                }
-            else
-                {
-                   openSchedule()
-                }
-        }
-
+      
         function entryDropdown()
         {
 
@@ -258,20 +132,14 @@
         {
             phoneMenu = true;            
             navHeight();
-            checkMenu();
             document.querySelector(".dropdown-content").style.transition="height 0.5s";
-            document.querySelector(".schedule-dropdown").style.transition="height 0.5s";
             document.querySelector(".entry-dropdown").style.transition="height 0.5s";
             
         }
 
         function navHeight()
         {
-            if(scheduleMenu && entryMenu)
-            {
-                document.querySelector("nav").style.height="552px";
-            }
-            else if(scheduleMenu || entryMenu)
+            if(entryMenu)
             {
                 document.querySelector("nav").style.height="460px";
             }
@@ -292,7 +160,7 @@
 
 
         window.addEventListener("resize", ()=>{
-            if(window.innerWidth >= 900)
+            if(window.innerWidth >= 1100)
             {
                 document.querySelector("nav").style.height="";
                 document.querySelector(".trigram").style.fillOpacity="100%";
